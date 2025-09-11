2c9d03d223d7a9a81994b907706470b45fe986fb
Kevin <carlisle1147@gmail.com>
2025-09-10 20:02:35 -0400

    Refactor auction scraper to use Botasaurus @browser with create_stealth_driver

--- scraper/auction_sites.py
+++ scraper/auction_sites.py
@@ -1,24 +1,25 @@
-from botasaurus_driver import Driver
+from botasaurus import browser, create_stealth_driver
 from bs4 import BeautifulSoup
-import time
 
 
-def scrape_storagetreasures():
-    url = "https://www.storagetreasures.com/auctions"
+@browser(create_driver=create_stealth_driver(start_url="https://www.storagetreasures.com/auctions"))
+def scrape_storagetreasures(driver, data):
+    """
+    Scrape auctions from StorageTreasures using a stealth driver.
+    """
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+
     auctions = []
+    for card in soup.select(".auction-card"):
+        try:
+            title = card.select_one(".auction-title").text.strip()
+            price = card.select_one(".current-bid").text.strip()
+            link = "https://www.storagetreasures.com" + \
+                card.select_one("a")["href"]
+            auctions.append({"title": title, "price": price, "link": link})
+        except Exception:
+            continue
 
-    with Driver(stealth=True) as driver:
-        driver.get(url)
-        time.sleep(5)
-        soup = BeautifulSoup(driver.page_source, "html.parser")
-
-        for card in soup.select(".auction-card"):
-            try:
-                title = card.select_one(".auction-title").text.strip()
-                price = card.select_one(".current-bid").text.strip()
-                link = "https://www.storagetreasures.com" + card.select_one("a")["href"]
-                auctions.append({"title": title, "price": price, "link": link})
-            except Exception:
-                continue
+    return auctions
 
     return auctions
